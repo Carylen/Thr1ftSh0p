2531164ef7c5c17495af0b50c411b87b90706b1d
Irham Zharfan <rayabadrs@gmail.com>
2023-06-10 00:06:32 +0700

    Test123

--- SourceJS/profile.js
+++ SourceJS/profile.js
@@ -3,18 +3,21 @@ const list2 = document.getElementById("list2")
 const list3 = document.getElementById("list3")
 const detailInfo = document.querySelector(".personalProfile-container")
 const usrLoc = document.querySelector(".userLocation-container")
+const changePass = document.querySelector(".changePass-container")
 
 list.addEventListener("click",
     e => { 
     detailInfo.style.display = "flex";
     usrLoc.style.display = "none"
+    changePass.style.display = "none"
     }
 );
 
 list2.addEventListener("click",
     e => { 
         detailInfo.style.display = "none"
-        usrLoc.style.display = "flex"
+        usrLoc.style.display = "block"
+        changePass.style.display = "none"
     }
 );
 
@@ -22,6 +25,7 @@ list3.addEventListener("click",
     e => { 
         detailInfo.style.display = "none"
         usrLoc.style.display = "none"
+        changePass.style.display = "block"
     }
 )
 
